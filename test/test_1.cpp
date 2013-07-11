b81f1462aefaedfe890724624fb0f04042cfc008
Mario S. Könz <mskoenz@gmx.net>
2013-07-11 14:29:02 -0400

    added test_1.cpp

---
+++ test/test_1.cpp
@@ -0,0 +1,12 @@
+// Author:  Mario S. Könz <mskoenz@gmx.net>
+// Date:    10.07.2013 15:40:10 EDT
+// File:    test_1.cpp
+
+#include <iostream>
+#include <sim_class.hpp>
+
+int main(int argc, char* argv[]) {
+    
+    
+    return 0;
+}
