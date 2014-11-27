d4ee09e01dadb9a234edf8e64b76fd9eab05cced
mskoenz <mskoenz@gmx.net>
2014-11-27 10:16:52 +0100

    added plot tools

---
+++ plot/convert.sh
@@ -0,0 +1,75 @@
+convert svg/hex_base_tile.svg png/hex_base_tile.png
+convert svg/hex_all_tile.svg png/hex_all_tile.png
+convert svg/hex_nbr_tile.svg png/hex_nbr_tile.png
+
+convert svg/tri_base_tile.svg png/tri_base_tile.png
+convert svg/tri_all_tile.svg png/tri_all_tile.png
+convert svg/tri_nbr_tile.svg png/tri_nbr_tile.png
+
+convert svg/sqr_base_tile.svg png/sqr_base_tile.png
+convert svg/sqr_all_tile.svg png/sqr_all_tile.png
+convert svg/sqr_nbr_tile.svg png/sqr_nbr_tile.png
+
+convert svg/tri_trans_1.svg png/tri_trans_1.png
+convert svg/tri_trans_2.svg png/tri_trans_2.png
+convert svg/tri_trans_3.svg png/tri_trans_3.png
+
+convert svg/sqr_trans_1.svg png/sqr_trans_1.png
+convert svg/sqr_trans_2.svg png/sqr_trans_2.png
+convert svg/sqr_trans_3.svg png/sqr_trans_3.png
+
+convert svg/hex_trans_1.svg png/hex_trans_1.png
+convert svg/hex_trans_2.svg png/hex_trans_2.png
+convert svg/hex_trans_3.svg png/hex_trans_3.png
+
+convert svg/sqr_trans_graph_1.svg png/sqr_trans_graph_1.png
+convert svg/sqr_trans_graph_2.svg png/sqr_trans_graph_2.png
+convert svg/sqr_trans_graph_3.svg png/sqr_trans_graph_3.png
+
+convert svg/sqr_swap_1.svg png/sqr_swap_1.png
+convert svg/sqr_swap_2.svg png/sqr_swap_2.png
+convert svg/sqr_swap_3.svg png/sqr_swap_3.png
+
+convert svg/tri_sign_1.svg png/tri_sign_1.png
+convert svg/tri_sign_2.svg png/tri_sign_2.png
+convert svg/tri_sign_3.svg png/tri_sign_3.png
+
+convert svg/sqr_sign_1.svg png/sqr_sign_1.png
+
+convert svg/sqr_mapping_1.svg png/sqr_mapping_1.png
+convert svg/sqr_mapping_2.svg png/sqr_mapping_2.png
+convert svg/sqr_mapping_3.svg png/sqr_mapping_3.png
+convert svg/sqr_mapping_4.svg png/sqr_mapping_4.png
+convert svg/sqr_mapping_5.svg png/sqr_mapping_5.png
+convert svg/sqr_mapping_6.svg png/sqr_mapping_6.png
+
+convert svg/sqr_update_1.svg png/sqr_update_1.png
+convert svg/sqr_update_2.svg png/sqr_update_2.png
+convert svg/sqr_update_3.svg png/sqr_update_3.png
+convert svg/sqr_update_4.svg png/sqr_update_4.png
+convert svg/sqr_update_5.svg png/sqr_update_5.png
+convert svg/sqr_update_6.svg png/sqr_update_6.png
+convert svg/sqr_update_7.svg png/sqr_update_7.png
+
+convert svg/winding_number_sqr_1.svg png/winding_number_sqr_1.png
+convert svg/winding_number_sqr_2.svg png/winding_number_sqr_2.png
+convert svg/winding_number_sqr_3.svg png/winding_number_sqr_3.png
+
+convert svg/winding_number_tri_1.svg png/winding_number_tri_1.png
+convert svg/winding_number_tri_2.svg png/winding_number_tri_2.png
+convert svg/winding_number_tri_3.svg png/winding_number_tri_3.png
+
+convert svg/sqr_shift_growth_0.svg png/sqr_shift_growth_0.png
+convert svg/sqr_shift_growth_1.svg png/sqr_shift_growth_1.png
+convert svg/sqr_shift_growth_2.svg png/sqr_shift_growth_2.png
+convert svg/sqr_shift_growth_3.svg png/sqr_shift_growth_3.png
+convert svg/sqr_shift_growth_4.svg png/sqr_shift_growth_4.png
+convert svg/sqr_shift_growth_5.svg png/sqr_shift_growth_5.png
+convert svg/sqr_shift_growth_6.svg png/sqr_shift_growth_6.png
+convert svg/sqr_shift_growth_7.svg png/sqr_shift_growth_7.png
+convert svg/sqr_shift_growth_8.svg png/sqr_shift_growth_8.png
+convert svg/sqr_shift_growth_9.svg png/sqr_shift_growth_9.png
+convert svg/sqr_shift_growth_10.svg png/sqr_shift_growth_10.png
+convert svg/sqr_shift_growth_11.svg png/sqr_shift_growth_11.png
+convert svg/sqr_shift_growth_12.svg png/sqr_shift_growth_12.png
+
